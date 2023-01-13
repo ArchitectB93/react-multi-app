a705379dc94c620f60dc845b8dd3fb944ff9e0f8
ArchitectB93 <107737895+ArchitectB93@users.noreply.github.com>
2023-01-13 11:26:20 -0800

    z

---
+++ src/components/Home.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import logo from './logo.png';
+
+console.log(logo);
+
+function Home() {
+  return (
+    <div className="home">
+      <div class="container">
+        <div class="row align-items-center my-5">
+          <div class="col-lg-7">
+            <img
+              class="img-fluid rounded mb-4 mb-lg-0"
+              src="http://placehold.it/900x400"
+              alt=""
+            />
+          </div>
+          <div>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;

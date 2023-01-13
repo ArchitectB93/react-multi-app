f194be4bfd581e9feca0e3e67ef2760ee8210dcf
ArchitectB93 <107737895+ArchitectB93@users.noreply.github.com>
2023-01-13 11:23:32 -0800

    zz

---
+++ src/components/About.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="about">
+      <div class="container">
+        <div class="my-5">
+          
+          <div>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
